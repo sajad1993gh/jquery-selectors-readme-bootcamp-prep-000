1c9ccf190470924f74b094ca57e1a3fd7d63a900
Sajad Gholamzadehrizi <sajad1993gh@gmail.com>
2019-01-21 00:29:16 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -5,3 +5,4 @@
 //Descendant Selector:  $('ul li')
 //First Child:          $('parent-tag child-tag:first-child') . $('div img#baby-ninja')
 //Alt Tag Selector:   $("img[alt='the beatles making faces']")
+//
