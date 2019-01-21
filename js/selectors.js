208775414832270173a93127cce57258e797c614
Sajad Gholamzadehrizi <sajad1993gh@gmail.com>
2019-01-21 00:25:32 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -3,5 +3,5 @@
 //Class selector:        $('.pics')
 //ID selector:           $('#baby-ninja')
 //Descendant Selector:  $('ul li')
-//First Child:          $('parent-tag child-tag:first-child')
+//First Child:          $('parent-tag child-tag:first-child') . $('div img:first-child')
 //
