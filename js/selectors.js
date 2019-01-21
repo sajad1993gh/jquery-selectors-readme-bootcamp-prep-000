1267ffe5907149668d62294f62e290ba0e9eab8f
Sajad Gholamzadehrizi <sajad1993gh@gmail.com>
2019-01-21 00:24:53 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,6 +1,6 @@
 // declare your functions here...
 //element selector(tag): $('img')
-//Class selector:  $('.pics')
-//ID selector:     $('#baby-ninja')
-//Descendant Selector: $('ul li')
-//First Child:         $('parent-tag child-tag:first-child')
+//Class selector:        $('.pics')
+//ID selector:           $('#baby-ninja')
+//Descendant Selector:  $('ul li')
+//First Child:          $('parent-tag child-tag:first-child')
